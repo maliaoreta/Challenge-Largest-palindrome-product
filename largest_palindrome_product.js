30a0cf120253868f41f777fa3a223138dfab26d4
maliaoreta <maliaoreta@gmail.com>
2016-03-15 19:34:14 -1000

    finished

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -10,8 +10,22 @@ module.exports = function(digits){
   var factor_1 = 0;
   var palindromeNumber = 0;
 
-  // do your work here
+  var product = 0;
 
+if (digits === 2) {
+  factor_0 = 99;
+  factor_1 = 99;
+
+  product = factor_0 * factor_1;
+  palindromeNumber = product.toString().split('').reverse();
+  palindromeNumber = Number(palindromeNumber.join(''));
+
+  while (product !== palindromeNumber){
+    factor_0 -= 1;
+    product = factor_0 * factor_1;
+    palindromeNumber = product.toString().split('').reverse();
+    palindromeNumber = Number(palindromeNumber.join(''));   
+  }
 
 
   return {
@@ -19,4 +33,31 @@ module.exports = function(digits){
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
   };
+
+} 
+
+else {
+  var reverse;
+    for (var i = 900; i < 1000; i++) {
+      for (var k = i; k < 1000; k++) {
+        product = k * i;
+        reverse = product.toString().split('').reverse();
+        reverse = Number(reverse.join(''));
+
+        if (product === reverse && product > palindromeNumber){
+          palindromeNumber = product;
+          console.log(product);
+        }
+      }
+    }
+
+
+
+    return {
+      factor_0 : factor_0,
+      factor_1 : factor_1,
+      palindromeNumber : palindromeNumber
+    };
+
+  }
 };
